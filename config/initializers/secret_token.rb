d66e28913d05b04246a3b7c5f2399ab3e324ac0a
Dig <dig.s.vargas@gmail.com>
2014-01-14 12:51:25 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyTrello::Application.config.secret_token = 'e9a88828e168bbab973cddc51b55f6fcd3bec7ba7430c89f998b3d984ddc389285e421267803b31357451b1773481e65a195d5811e54110dd6787135a5184d54'
