4aa38d3dfcc60b0b5a79ca721105d46fafa06157
Dig <dig.s.vargas@gmail.com>
2014-01-15 10:06:51 -0800

    Fixes db setup to allow for current user. Adds initial User model. Adds Annotate gem for better model tracking.

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string(255)      not null
+#  password_digest :string(255)      not null
+#  session_token   :string(255)      not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :password_digest, :session_token, :username
+end
